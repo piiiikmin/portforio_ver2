ed042f7d6475bbd5f6aa4534820f9415b15a0e7e
DESKTOP-UUVO7JG\sato <aimkitsou.014@gmail.com>
2025-02-16 18:39:51 +0900

    Favicon設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,18 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "I.SATO Portforio Website",
+  description: "フロントエンド好きなpiiiikminのポートフォリオサイト",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body>{children}</body>
     </html>
-  )
+  );
 }
