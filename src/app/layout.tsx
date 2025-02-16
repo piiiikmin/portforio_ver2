e97ad68e8f3cf709b10ab1501e49717fcb986b01
DESKTOP-UUVO7JG\sato <aimkitsou.014@gmail.com>
2025-02-16 22:08:45 +0900

    global.css、font-family設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,8 @@
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "@/app/styles/global.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "I.SATO Portforio Website",
@@ -12,7 +16,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
-      <body>{children}</body>
+      <body className={inter.className}>
+        { children }
+      </body>
     </html>
   );
 }
